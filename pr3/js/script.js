07a032e9b38a59b85e76a87ba092339fdfbe6a5d
312 <312@kep.nung.edu.ua>
2024-10-08 15:00:35 +0300

    pr3 ver 1

---
+++ pr3/js/script.js
@@ -0,0 +1,36 @@
+function dateConverterLogic() {
+    const dateInput = document.querySelector('#dateInput');
+    const dateDiv = document.querySelector('.date');
+    const p = dateDiv.querySelector('p');
+
+    dateInput.value = '25/12/2023 fefsfefe 24/10/2024';
+
+    function dateConvertor(dateInput) {
+        let dateString = dateInput.value;
+        const regexRes = dateString.match(/\d{2}\/\d{2}\/\d{4}/g)
+        if (regexRes) {
+            // const operationArray = [];
+            const resultArray = []
+            for (let i = 0; i < regexRes.length; i++) {
+                let tempArr = regexRes[i].split('/');
+                resultArray.push(`${tempArr[2]}-${tempArr[1]}-${tempArr[0]}`)
+                dateString = dateString.replace(/\d{2}\/\d{2}\/\d{4}/, resultArray[i])
+            }
+            p.textContent = dateString;
+        }
+        else {
+            p.textContent = dateString;
+        }
+    }
+
+    dateConvertor(dateInput)
+    dateInput.addEventListener('change', () => dateConvertor(dateInput));
+}
+
+dateConverterLogic()
+
+function emailMatch(){
+    const emailInput = document.querySelector('#emailInput');
+    const emailDiv = document.querySelector('')
+
+}
